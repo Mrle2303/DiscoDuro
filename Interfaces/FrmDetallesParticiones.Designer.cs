40b0f4cd0b0524dda84280630cf741ad3859132e
Mayi Perez <marlenec2303@gmail.com>
2022-03-09 18:22:17 -0700

    referencias

--- Interfaces/FrmDetallesParticiones.Designer.cs
+++ Interfaces/FrmDetallesParticiones.Designer.cs
@@ -29,21 +29,23 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.TxtNombreP = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
             this.TxtTamanioP = new System.Windows.Forms.TextBox();
+            this.TxtNombreP = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.btnAñadir = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnGuardar = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -58,30 +60,28 @@
             this.panel1.Size = new System.Drawing.Size(207, 96);
             this.panel1.TabIndex = 0;
             // 
-            // flowLayoutPanel1
+            // label7
             // 
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(244, 29);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(378, 239);
-            this.flowLayoutPanel1.TabIndex = 1;
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(172, 54);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 13);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "MB";
             // 
-            // label1
+            // TxtTamanioP
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(92, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Añadir Particiones";
+            this.TxtTamanioP.Location = new System.Drawing.Point(66, 51);
+            this.TxtTamanioP.Name = "TxtTamanioP";
+            this.TxtTamanioP.Size = new System.Drawing.Size(100, 20);
+            this.TxtTamanioP.TabIndex = 3;
             // 
-            // label2
+            // TxtNombreP
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(14, 19);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 13);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Nombre";
+            this.TxtNombreP.Location = new System.Drawing.Point(66, 12);
+            this.TxtNombreP.Name = "TxtNombreP";
+            this.TxtNombreP.Size = new System.Drawing.Size(100, 20);
+            this.TxtNombreP.TabIndex = 2;
             // 
             // label3
             // 
@@ -92,25 +92,29 @@
             this.label3.TabIndex = 1;
             this.label3.Text = "Tamaño";
             // 
-            // TxtNombreP
+            // label2
             // 
-            this.TxtNombreP.Location = new System.Drawing.Point(66, 12);
-            this.TxtNombreP.Name = "TxtNombreP";
-            this.TxtNombreP.Size = new System.Drawing.Size(100, 20);
-            this.TxtNombreP.TabIndex = 2;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nombre";
             // 
-            // TxtTamanioP
+            // label1
             // 
-            this.TxtTamanioP.Location = new System.Drawing.Point(66, 51);
-            this.TxtTamanioP.Name = "TxtTamanioP";
-            this.TxtTamanioP.Size = new System.Drawing.Size(100, 20);
-            this.TxtTamanioP.TabIndex = 3;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Añadir Particiones";
             // 
             // btnAñadir
             // 
-            this.btnAñadir.Location = new System.Drawing.Point(78, 132);
+            this.btnAñadir.Location = new System.Drawing.Point(78, 131);
             this.btnAñadir.Name = "btnAñadir";
-            this.btnAñadir.Size = new System.Drawing.Size(75, 23);
+            this.btnAñadir.Size = new System.Drawing.Size(78, 27);
             this.btnAñadir.TabIndex = 3;
             this.btnAñadir.Text = "Añadir ";
             this.btnAñadir.UseVisualStyleBackColor = true;
@@ -127,67 +131,79 @@
             // 
             // panel2
             // 
-            this.panel2.Controls.Add(this.label8);
-            this.panel2.Controls.Add(this.label6);
-            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.panel3);
             this.panel2.Location = new System.Drawing.Point(244, 29);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(378, 44);
+            this.panel2.Size = new System.Drawing.Size(378, 208);
             this.panel2.TabIndex = 0;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(21, 15);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(48, 13);
-            this.label5.TabIndex = 0;
-            this.label5.Text = "Partición";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(86, 15);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(46, 13);
-            this.label6.TabIndex = 1;
-            this.label6.Text = "Tamaño";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(172, 54);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(23, 13);
-            this.label7.TabIndex = 5;
-            this.label7.Text = "MB";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(151, 15);
+            this.label8.Location = new System.Drawing.Point(155, 15);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(56, 13);
             this.label8.TabIndex = 2;
             this.label8.Text = "Disponible";
             // 
-            // FrmTamanos
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(87, 15);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Tamaño";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Partición";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.label8);
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(378, 41);
+            this.panel3.TabIndex = 3;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(392, 241);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 5;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // FrmDetallesParticiones
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(737, 307);
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(646, 276);
+            this.Controls.Add(this.btnGuardar);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.btnAñadir);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.panel1);
-            this.Name = "FrmTamanos";
+            this.Name = "FrmDetallesParticiones";
             this.Text = "FrmTamanos";
+            this.Load += new System.EventHandler(this.FrmDetallesParticiones_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -196,7 +212,6 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.TextBox TxtTamanioP;
         private System.Windows.Forms.TextBox TxtNombreP;
         private System.Windows.Forms.Label label3;
@@ -209,5 +224,7 @@
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnGuardar;
     }
 }
--- Interfaces/FrmDetallesParticiones.cs
+++ Interfaces/FrmDetallesParticiones.cs
@@ -7,17 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Logica_de_negocios;
+
 
 namespace Interfaces
 {
     public partial class FrmDetallesParticiones : Form
     {
+        
         public FrmDetallesParticiones()
         {
             InitializeComponent();
+            
         }
 
         private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmDetallesParticiones_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
         {
 
         }
