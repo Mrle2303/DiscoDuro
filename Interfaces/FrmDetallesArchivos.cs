464c96df72f3f3bc0ab3f42468ee82afd4c90ba9
Francisco-Meza <L201140051@surnayarit.tecnm.mx>
2022-03-10 22:25:05 -0700

    Modifique el frm Archivos

--- Interfaces/FrmDetallesArchivos.cs
+++ Interfaces/FrmDetallesArchivos.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Logica_de_negocios;
 
 namespace Interfaces
 {
@@ -14,20 +15,17 @@ namespace Interfaces
     {
         
         private DataTable archivos;
-
-        public FrmDetallesArchivos()
+        private Particiones particion;
+        public FrmDetallesArchivos(Particiones particion)
         {
             InitializeComponent();
+            this.particion = particion;
             archivos = new DataTable();
-
             archivos.Columns.Add("Nombre del archivo");
             archivos.Columns.Add("Tamaño del archivo");
 
             dgvArchivos.DataSource=archivos;   
         }
-
-        
-
         private void btnAñadir_Click(object sender, EventArgs e)
         {
             DataRow row = archivos.NewRow();
