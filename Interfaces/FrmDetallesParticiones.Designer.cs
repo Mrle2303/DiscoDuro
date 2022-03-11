a306563c4faf0affe373da92bc583178fff46f2d
Mayi Perez <marlenec2303@gmail.com>
2022-03-10 18:14:09 -0700

    Cambios del formulario de particiones

--- Interfaces/FrmDetallesParticiones.Designer.cs
+++ Interfaces/FrmDetallesParticiones.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label7 = new System.Windows.Forms.Label();
             this.TxtTamanioP = new System.Windows.Forms.TextBox();
@@ -37,14 +38,12 @@
             this.label1 = new System.Windows.Forms.Label();
             this.btnAñadir = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.List1 = new System.Windows.Forms.ListBox();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
+            this.DgvInfoParticiones = new System.Windows.Forms.DataGridView();
+            this.NombreP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TamañoP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnBorar = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
-            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvInfoParticiones)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -128,57 +127,54 @@
             this.label4.TabIndex = 4;
             this.label4.Text = "Particiones Existentes";
             // 
-            // panel2
+            // DgvInfoParticiones
             // 
-            this.panel2.Controls.Add(this.List1);
-            this.panel2.Controls.Add(this.panel3);
-            this.panel2.Location = new System.Drawing.Point(244, 29);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(378, 208);
-            this.panel2.TabIndex = 0;
+            this.DgvInfoParticiones.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvInfoParticiones.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.NombreP,
+            this.TamañoP});
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.YellowGreen;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.DgvInfoParticiones.DefaultCellStyle = dataGridViewCellStyle1;
+            this.DgvInfoParticiones.Location = new System.Drawing.Point(244, 30);
+            this.DgvInfoParticiones.Name = "DgvInfoParticiones";
+            this.DgvInfoParticiones.Size = new System.Drawing.Size(250, 150);
+            this.DgvInfoParticiones.TabIndex = 5;
+           
             // 
-            // List1
+            // NombreP
             // 
-            this.List1.FormattingEnabled = true;
-            this.List1.Location = new System.Drawing.Point(3, 45);
-            this.List1.Name = "List1";
-            this.List1.Size = new System.Drawing.Size(180, 160);
-            this.List1.TabIndex = 4;
+            this.NombreP.HeaderText = "Nombre";
+            this.NombreP.Name = "NombreP";
             // 
-            // panel3
+            // TamañoP
             // 
-            this.panel3.Controls.Add(this.label5);
-            this.panel3.Controls.Add(this.label6);
-            this.panel3.Location = new System.Drawing.Point(0, 0);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(378, 41);
-            this.panel3.TabIndex = 3;
+            this.TamañoP.HeaderText = "Tamaño";
+            this.TamañoP.Name = "TamañoP";
             // 
-            // label5
+            // btnBorar
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(16, 15);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(48, 13);
-            this.label5.TabIndex = 0;
-            this.label5.Text = "Partición";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(109, 15);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(46, 13);
-            this.label6.TabIndex = 1;
-            this.label6.Text = "Tamaño";
+            this.btnBorar.Location = new System.Drawing.Point(328, 186);
+            this.btnBorar.Name = "btnBorar";
+            this.btnBorar.Size = new System.Drawing.Size(75, 23);
+            this.btnBorar.TabIndex = 6;
+            this.btnBorar.Text = "Borar";
+            this.btnBorar.UseVisualStyleBackColor = true;
+            this.btnBorar.Click += new System.EventHandler(this.btnBorar_Click);
             // 
             // FrmDetallesParticiones
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.ClientSize = new System.Drawing.Size(650, 285);
-            this.Controls.Add(this.panel2);
+            this.ClientSize = new System.Drawing.Size(520, 221);
+            this.Controls.Add(this.btnBorar);
+            this.Controls.Add(this.DgvInfoParticiones);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.btnAñadir);
             this.Controls.Add(this.label1);
@@ -187,9 +183,7 @@
             this.Text = "FrmTamanos";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            this.panel2.ResumeLayout(false);
-            this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvInfoParticiones)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -206,10 +200,9 @@
         private System.Windows.Forms.Button btnAñadir;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.ListBox List1;
+        private System.Windows.Forms.DataGridView DgvInfoParticiones;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NombreP;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TamañoP;
+        private System.Windows.Forms.Button btnBorar;
     }
 }
